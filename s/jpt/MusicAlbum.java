aa00d0440281e16b7076d27f51cafdcf622f1c96
Oksana Bozhenko <oksanabozhenko1304@gmail.com>
2016-04-26 16:07:33 +0300

    Added xml parser

---
+++ s/jpt/MusicAlbum.java
@@ -0,0 +1,22 @@
+package jpt;
+
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class MusicAlbum {
+	
+	public static void main(String[] args) {
+		Parser parser = new Parser();
+		parser.parseXml();
+	}
+
+}
